8ae3169bef50ef328dd16cb873df191e62f2a2b8
aiswarya1311 <78871076+aiswarya1311@users.noreply.github.com>
2021-03-12 12:28:05 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyDRdt-w8NRqQBtMKON6YKOj8FDsk35tejc",
+    authDomain: "todolist-7a104.firebaseapp.com",
+    databaseURL: "https://todolist-7a104-default-rtdb.firebaseio.com",
+    projectId: "todolist-7a104",
+    storageBucket: "todolist-7a104.appspot.com",
+    messagingSenderId: "1072373691066",
+    appId: "1:1072373691066:web:6cc12460251eb9a20d4883",
+    measurementId: "G-HM29HYD8YN"
+  }
 };
